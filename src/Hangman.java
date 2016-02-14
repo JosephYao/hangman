99f97e16375e3dcd6abfd8801ab2132c17dc0463
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-02-14 21:24:14 +0800

    length of word when game start

---
+++ src/Hangman.java
@@ -0,0 +1,11 @@
+public class Hangman {
+    private final String word;
+
+    public Hangman(String word) {
+        this.word = word;
+    }
+
+    public int length() {
+        return word.length();
+    }
+}
---
+++ src/TestHangman.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestHangman {
+
+    @Test
+    public void length_of_word_when_game_start() {
+        Hangman hangman = new Hangman("word");
+
+        assertEquals(4, hangman.length());
+    }
+}
