e45e0a8037fe0b666d5b76d8718dba3372fb3961
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-02-15 22:17:57 +0800

    game is not over when game start

--- test/cn/codingstyle/hangman/TestHangmanGameOver.java
+++ test/cn/codingstyle/hangman/TestHangmanGameOver.java
@@ -3,17 +3,25 @@ package cn.codingstyle.hangman;
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 public class TestHangmanGameOver {
 
     HangmanHelper hangman = new HangmanHelper();
+    private final Runnable mockWhenGameOver = mock(Runnable.class);
+
+    @Test
+    public void game_is_not_over_when_game_start() {
+        hangman.whenGameOver(mockWhenGameOver);
+
+        verify(mockWhenGameOver, never()).run();
+    }
 
     @Test
     public void game_is_over_when_type_a_char_after_all_tries_used() {
         hangman.typeAnyCharAfterAllTriesUsed();
 
-        Runnable mockWhenGameOver = mock(Runnable.class);
         hangman.whenGameOver(mockWhenGameOver);
 
         verify(mockWhenGameOver).run();
