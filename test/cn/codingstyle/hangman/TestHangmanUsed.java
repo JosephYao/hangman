77d594f56d65874b9862dcca7a821668423e5b01
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-02-15 23:19:35 +0800

    used is still all vowel chars when type vowel char

--- test/cn/codingstyle/hangman/TestHangmanUsed.java
+++ test/cn/codingstyle/hangman/TestHangmanUsed.java
@@ -5,11 +5,20 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class TestHangmanUsed {
+
+    private static final String ALL_VOWEL_CHARS = "aeiou";
+    private static final char VOWEL = 'a';
+    Hangman hangman = new Hangman("word");
+
+    @Test
+    public void used_is_all_vowel_chars_when_game_start() {
+        assertEquals(ALL_VOWEL_CHARS, hangman.used());
+    }
     
     @Test
-    public void used_is_aeiou_when_game_start() {
-        Hangman hangman = new Hangman("word");
-        
-        assertEquals("aeiou", hangman.used());
+    public void used_is_still_all_vowel_chars_when_type_vowel_char() {
+        hangman.type(VOWEL);
+
+        assertEquals(ALL_VOWEL_CHARS, hangman.used());
     }
 }
