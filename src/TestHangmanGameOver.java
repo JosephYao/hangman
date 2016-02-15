2362ad66788ebf5ea5fd515a50fb09d4334e0597
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-02-15 14:38:04 +0800

    move game is over test to a new test class

---
+++ src/TestHangmanGameOver.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import java.util.stream.IntStream;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class TestHangmanGameOver {
+
+    Hangman hangman = new Hangman("word");
+    private static final char ANY_CHAR = 'a';
+    private static final int MAX_TRIES = 12;
+
+    @Test
+    public void game_is_over_when_type_a_char_after_all_tries_used() {
+        allTriesUsed();
+        Runnable mockWhenGameOver = mock(Runnable.class);
+        hangman.type(ANY_CHAR);
+
+        hangman.whenGameOver(mockWhenGameOver);
+
+        verify(mockWhenGameOver).run();
+    }
+
+    private void allTriesUsed() {
+        IntStream.range(0, MAX_TRIES).forEach(i -> {
+            hangman.type(ANY_CHAR);
+        });
+    }
+}
--- src/TestHangmanTries.java
+++ src/TestHangmanTries.java
@@ -3,8 +3,6 @@ import org.junit.Test;
 import java.util.stream.IntStream;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 public class TestHangmanTries {
 
@@ -33,19 +31,8 @@ public class TestHangmanTries {
         assertEquals(0, hangman.tries());
     }
 
-    @Test
-    public void game_is_over_when_type_a_char_after_all_tries_used() {
-        allTriesUsed();
-        Runnable mockWhenGameOver = mock(Runnable.class);
-        hangman.type(ANY_CHAR);
-
-        hangman.whenGameOver(mockWhenGameOver);
-
-        verify(mockWhenGameOver).run();
-    }
-
     private void allTriesUsed() {
-        IntStream.range(0, 12).forEach(i -> {
+        IntStream.range(0, MAX_TRIES).forEach(i -> {
             hangman.type(ANY_CHAR);
         });
     }
