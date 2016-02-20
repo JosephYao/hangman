678a9c93e6ba8146ad8d0f75b68aa2fd254eadfe
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-02-20 13:42:45 +0800

    refactor HangmanHelper

--- test/cn/codingstyle/hangman/HangmanHelper.java
+++ test/cn/codingstyle/hangman/HangmanHelper.java
@@ -4,7 +4,7 @@ import java.util.stream.IntStream;
 
 public class HangmanHelper {
     public static final int MAX_TRIES = 12;
-    private static final char ANY_CHAR = 'a';
+    private static final char ANY_CHAR = 'x';
     private static final char NOT_CONTAINED_CHAR = 'z';
 
     private final Hangman hangman;
@@ -28,7 +28,7 @@ public class HangmanHelper {
 
     public void typeAnyCharAfterAllTriesUsed() {
         allTriesUsed();
-        typeNotContainedChar();
+        type(ANY_CHAR);
     }
 
     public int tries() {
