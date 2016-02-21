83ec92225dfeb9ff621fd1934e08ba12f813fe4e
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-02-21 20:46:11 +0800

    discovered when type a contained char

--- test/cn/codingstyle/hangman/TestHangmanDiscoveredWhenType.java
+++ test/cn/codingstyle/hangman/TestHangmanDiscoveredWhenType.java
@@ -6,12 +6,19 @@ import static org.junit.Assert.assertEquals;
 
 public class TestHangmanDiscoveredWhenType {
 
+    HangmanHelper hangman = new HangmanHelper("word");
+
     @Test
     public void discovered_when_type_a_not_contained_char() {
-        HangmanHelper hangman = new HangmanHelper("word");
-
         hangman.typeButNotCheckGameOver('z');
 
         assertEquals("_o__", hangman.discovered());
+    }
+
+    @Test
+    public void discovered_when_type_a_contained_char() {
+        hangman.typeButNotCheckGameOver('w');
+
+        assertEquals("wo__", hangman.discovered());
     }
 }
