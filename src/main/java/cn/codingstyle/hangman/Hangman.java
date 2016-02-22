cad0848cb5adece2f145a3ae452cb84431f11749
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-02-22 22:34:18 +0800

    tries remain unchanged when type a contained consonant

--- src/main/java/cn/codingstyle/hangman/Hangman.java
+++ src/main/java/cn/codingstyle/hangman/Hangman.java
@@ -21,11 +21,12 @@ public class Hangman {
 
     public void type(char c) {
         appendCharToUsed(c);
-        decreaseTries();
+        decreaseTries(c);
     }
 
-    private void decreaseTries() {
-        tries--;
+    private void decreaseTries(char c) {
+        if (ALL_VOWEL.indexOf(c) > -1)
+            tries--;
     }
 
     private void appendCharToUsed(char c) {
