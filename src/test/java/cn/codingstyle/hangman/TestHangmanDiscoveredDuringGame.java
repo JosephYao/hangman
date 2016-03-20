18dcff9f4aece9f2b5f5fa19b8ce53bfa8d511ed
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-03-20 18:00:23 +0800

    remove unnecessary test and refactoring with HangmanForTest

--- src/test/java/cn/codingstyle/hangman/TestHangmanDiscoveredDuringGame.java
+++ src/test/java/cn/codingstyle/hangman/TestHangmanDiscoveredDuringGame.java
@@ -7,20 +7,12 @@ import static org.junit.Assert.assertEquals;
 public class TestHangmanDiscoveredDuringGame {
 
     @Test
-    public void discovered_when_type_the_only_one_contained_consonant() {
-        Hangman hangman = new Hangman("am");
+    public void discovered_when_type_a_contained_consonant() {
+        HangmanForTest hangman = new HangmanForTest("and");
 
-        hangman.type('m', ()->{});
-
-        assertEquals("am", hangman.discovered());
-    }
-
-    @Test
-    public void discovered_when_type_a_contained_consonant_but_not_the_only_one() {
-        Hangman hangman = new Hangman("and");
-
-        hangman.type('n', ()->{});
+        hangman.typeWithoutCheckGameLost('n');
 
         assertEquals("an_", hangman.discovered());
     }
+
 }
