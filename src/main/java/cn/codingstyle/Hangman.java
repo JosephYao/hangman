997fd2c817df1844ba91dbc5586455dccb03649f
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-03-29 16:31:43 +0800

    length of word

---
+++ src/main/java/cn/codingstyle/Hangman.java
@@ -0,0 +1,13 @@
+package cn.codingstyle;
+
+public class Hangman {
+    private final String word;
+
+    public Hangman(String word) {
+        this.word = word;
+    }
+
+    public int length() {
+        return word.length();
+    }
+}
