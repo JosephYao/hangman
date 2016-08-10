b6f688b996fcba916354051f5e0f51b3371bbfc2
Joseph Yao <joseph.yao.ruozhou@gmail.com>
2016-08-10 21:50:01 +0800

    Refactoring: turn typed list from char to string, code is simpler

--- src/com/odde/Hangman.java
+++ src/com/odde/Hangman.java
@@ -1,26 +1,25 @@
 package com.odde;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
 
 public class Hangman {
     private static final int MAX_TRIES = 12;
     private static final String ALL_VOWELS = "aeiou";
     private static final String PLACEHOLDER = "_";
     private final String word;
-    private List<Character> typed = allVowels();
+    private List<String> typed = allVowels();
 
     public Hangman(String word) {
         this.word = word;
     }
 
-    private List<Character> allVowels() {
-        return ALL_VOWELS.chars()
-                .mapToObj(i -> (char)i)
-                .collect(toList());
+    private List<String> allVowels() {
+        return new ArrayList<>(Arrays.asList(ALL_VOWELS.split("")));
     }
 
     public int length() {
@@ -29,21 +28,20 @@ public class Hangman {
 
     public String used() {
         return uniqueTypedChars()
-                .map(Object::toString)
                 .collect(joining());
     }
 
     public void type(char c, Runnable afterGameOver) {
-        typed.add(c);
+        typed.add(String.valueOf(c));
         if (tries() == 0)
             afterGameOver.run();
     }
 
     public int tries() {
-        return MAX_TRIES - numberOfFailedTypes();
+        return MAX_TRIES - numberOfWrongTypes();
     }
 
-    private int numberOfFailedTypes() {
+    private int numberOfWrongTypes() {
         return typed.size() - ALL_VOWELS.length() - numberOfCorrectConsonants();
     }
 
@@ -53,33 +51,29 @@ public class Hangman {
                 .count();
     }
 
-    private Stream<Character> uniqueTypedChars() {
+    private Stream<String> uniqueTypedChars() {
         return typed.stream().distinct();
     }
 
-    private boolean isCorrectConsonant(Character c) {
+    private boolean isCorrectConsonant(String c) {
         return isConsonant(c) && isPartOfWord(c);
     }
 
-    private boolean isPartOfWord(Character c) {
-        return word.indexOf(c) >= 0;
+    private boolean isPartOfWord(String c) {
+        return word.contains(c);
     }
 
-    private boolean isConsonant(Character c) {
-        return ALL_VOWELS.indexOf(c) == -1;
+    private boolean isConsonant(String c) {
+        return !ALL_VOWELS.contains(c);
     }
 
     public String discovered() {
-        return word.chars()
-                .mapToObj(this::discoveredChar)
+        return Arrays.stream(word.split(""))
+                .map(this::discoveredChar)
                 .collect(joining());
     }
 
-    private String discoveredChar(int i) {
-        char c = (char) i;
-        if (typed.contains(c))
-            return String.valueOf(c);
-        else
-            return PLACEHOLDER;
+    private String discoveredChar(String c) {
+        return typed.contains(c) ? c : PLACEHOLDER;
     }
 }
